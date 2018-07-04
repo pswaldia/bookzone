f73535da4f6343c292dd01f70035baac53c14b3d
pswaldia <pswaldia1@gmail.com>
2018-07-05 01:10:08 +0530

    added new configuration

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-basedir=os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
-	SECRET_KEY=	os.environ.get('SECRET_KEY') or "you-will-never-guess-that"
-	SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite///'+os.path.join(basedir,'app.db')
-	#flask_sqlalchemy stores the application database location
-	SQLACHEMY_TRACK_MODIFICATIONS=False 
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
