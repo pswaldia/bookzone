f5b4283e925595ca74fa90cda123adf395dd4687
pswaldia <pswaldia1@gmail.com>
2018-07-05 01:11:32 +0530

    commented out the mistake

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config   #this is an important part in any flask applications
+
 
 app=Flask(__name__)
+app.config.from_object(Config)    #this must be defined before instantiation of db and migrate
 
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 
-from config import Config   #this is an important part in any flask applications
 
-app.config.from_object(Config)
 
-from app import routes
+
+
+from app import routes,models
