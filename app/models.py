566a96084dfb8989d54636f1f55c16ed7ccf9e0e
pswaldia <pswaldia1@gmail.com>
2018-07-05 01:31:41 +0530

    added a new database model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
 from app import db
+from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts=db.relationship('Post',backref='author',lazy='dynamic')
+    #the posts is a high level view of the relation bw the user and post , it is not the exact datafield
+    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)  #for debug purpose
+
+class Post(db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	body=db.Column(db.String(140))
+	timestamp=db.Column(db.DateTime,index=True,default=datetime.utcnow)
+	user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Post {}>'.format(self.body)
