7a2e7c78d6ad512b6699c5802112f54a716c0dcd
pswaldia <pswaldia1@gmail.com>
2018-07-05 17:18:27 +0530

    added flask shell context

--- bookzone.py
+++ bookzone.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app,db
+from app.models import User,Post
+
+@app.shell_context_processor       #this makes it possile to write flask shell, that opens a python interpretor in context of the flask application (present one)
+def make_shell_context():
+	return {'db':db,'User':User,'Post':Post}
