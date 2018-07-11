ea0d3287d1a7d03ec0c301c5a876e8d115bc3185
pswaldia <pswaldia1@gmail.com>
2018-07-11 20:34:39 +0530

    added functionality for login as well register

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,EqualTo,ValidationError
+from app.models import User
 
 class RegistrationForm(FlaskForm):
 	username=StringField('Username',validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,64 @@
-from app import app
-from flask import render_template,redirect,flash
+from app import app,db
+from flask import render_template,redirect,flash,url_for
 from app.forms import RegistrationForm,LoginForm
 from flask_login import current_user,login_user,logout_user
 from app.models import User
 import pandas as pd
 df=pd.read_csv("/Users/pswaldia1/bookzone/app/static/goodread.csv")
-@app.route("/",methods=['GET','POST'])
-def index():
-	registerForm=RegistrationForm()
-	
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form=LoginForm()
 	if current_user.is_authenticated:
+		return redirect(url_for('logout'))
+	if form.validate_on_submit():
+		user=User.query.filter_by(username = form.username.data).first()
+		if user is None or not  user.check_password(form.password.data):
+			flash('Invalid Username or Password')
+			return redirect(url_for('login'))
+		login_user(user,remember=form.remember_me.data)
 		return render_template('profile.html')
-	loginForm=LoginForm()
-	if loginForm.validate_on_submit():
-		user=User.query.filter_by(username=loginForm.username.data).first()
-		if user is None or not user.check_password(loginForm.password.data):
-			flash('Invalid username or password')
-			return redirect('/')
-		login_user(user,remember=loginForm.remember_me.data)  #this handles the login of the user 
-		return redirect('/')
-	return render_template('forms.html',form2=loginForm,form1=registerForm,df=df)		
-@app.route('/profile')
+	return render_template('login.html',form=form)	
+
+
+@app.route('/logout')
 def logout():
     logout_user()
-    return redirect('/')
+    return redirect(url_for('register'))
 
+
+
+
+
+@app.route('/',methods=['GET','POST'])
+@app.route("/register",methods=['GET','POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('logout'))
+	registerForm=RegistrationForm()
+	loginForm=LoginForm()
+	if registerForm.validate_on_submit():
+		u=User(username=registerForm.username.data,email=registerForm.email.data)
+		u.set_password(registerForm.password.data)
+		db.session.add(u)
+		db.session.commit()
+		flash('congratulations! You are now registered!')
+		return redirect(url_for('register'))
+	return render_template('forms.html',form1=registerForm,df=df)
+
+
+
+        
+
+
+        
+        
+        
+       
+
+	
+		
 
 
 
